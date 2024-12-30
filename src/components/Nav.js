809511d2c7c9253450cd7d977ebd5c1a55fafcaf
Anishabaral <130526545+Anishabaral@users.noreply.github.com>
2024-12-30 13:37:08 +0530

    added searchbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import "./Nav.css"
-
+import axios from 'axios';
+import requests from './Requests';
+import { SearchContext } from '../App';
 
 
 function Nav() {
     const [show, handleShow] = useState(false);
+    const [searchText, setSearchText] = useState("");
+    const cancelTokenRef = useRef(null);
+    const [searchedData, setSearchedData] = useContext(SearchContext)
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -20,6 +25,35 @@ function Nav() {
             });
         };
     }, []);
+    const searchMovies = async (query) => {
+        if (cancelTokenRef.current) {
+            cancelTokenRef.current.cancel("Canceled previous request");
+        }
+        cancelTokenRef.current = axios.CancelToken.source();
+        try {
+            const res = await axios.get(`${requests.searchMovies}&query=${query}`, {
+                cancelToken: cancelTokenRef.current.token,
+            });
+            setSearchedData(prevList => [...res.data.results]);
+        } catch (error) {
+            if (axios.isCancel(error)) {
+                console.log("Request canceled:", error.message);
+            } else {
+                console.error("API error:", error);
+            }
+        }
+    };
+    const handleSearch = (event) => {
+        console.log(event ,event.target.value)
+        const query = event.target.value;
+        setSearchText(query);
+        if (query) {
+            setTimeout(() => searchMovies(query), 2000); // Adding debounce
+        }
+        if(!query){
+            setSearchedData(prevList => [...prevList.splice(0, prevList.length)]);
+        }
+    };
 
     return (
         <div className={`nav ${show && "nav_black"}`}>
@@ -28,11 +62,13 @@ function Nav() {
                 src={require(`./logo.png`)}
                 alt="Netflix Logo"
             />
-            <img 
-                className="nav_avatar"
-                src={require(`./avatar.png`)}
-                alt="Netflix Avatar"
-            />
+           <div className='nav_search'>
+                <input className='search_bar' type='text' value={searchText} onChange={handleSearch} placeholder='search your movie' />
+                <img
+                    src={require(`./avatar.png`)}
+                    alt="Netflix Avatar"
+                />
+            </div>
         </div>
     )
 }
